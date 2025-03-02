dc502fa8b284ac2dd103a0eba9a92077154d65c5
Rulft44 <dimucciorubengermania@gmail.com>
2025-03-02 16:34:07 +0000

    :tada: Welcome sigmas!

---
+++ src/main/java/net/sigmarizzler/steamy/Steamy.java
@@ -0,0 +1,19 @@
+package net.sigmarizzler.steamy;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Steamy implements ModInitializer {
+	public static final String MOD_ID = "steamy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("We rizzing up baddies with this one! :fire: trolololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololololol");
+
+		//skibidi
+
+	}
+}
---
+++ src/main/java/net/sigmarizzler/steamy/SteamyClient.java
@@ -0,0 +1,10 @@
+package net.sigmarizzler.steamy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SteamyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
